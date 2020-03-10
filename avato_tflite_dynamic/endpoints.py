01e1dd24d24213437093f63cb4246fc5e3268816
Enrico Ghirardi <i@choco.me>
2020-03-10 15:00:38 +0100

    use new avato proto

--- avato_tflite_dynamic/endpoints.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-from avato.api import AVATO_ACTIVE_INSTANCE_INFIX
-
-
-class Endpoints(str, Enum):
-    # TFLITE Dynamic Instance specific
-    POST_DYNAMIC_MESSAGE = AVATO_ACTIVE_INSTANCE_INFIX + "/dynamicMessage"
